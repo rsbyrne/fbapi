a10ebcde72139272c36ec6f776944cd1dbabc4d9
rsbyrne <rohan.byrne@gmail.com>
2020-04-28 00:42:43 +0000

    Tweaks.

--- run.sh
+++ run.sh
@@ -2,12 +2,13 @@
 dataURL=$1
 loginName=$2
 loginPass=$3
-outDir=${4:-'.'}
+outDir=${4:-$PWD}
 dataMime=${5:-'text/csv'}
 outExt=${6:-'.csv'}
-MOUNTFROM=$PWD
+MOUNTFROM=$outDir
 MOUNTTO='/mnt'
 IMAGE='rsbyrne/fbapi'
 SCRIPTPATH='/fbapi/run.py'
+mkdir -p $outDir
 docker run -v $MOUNTFROM:$MOUNTTO -it --shm-size 2g $IMAGE \
-  python $SCRIPTPATH $dataURL $loginName $loginPass $MOUNTTO/$outDir $dataMime $outExt
+  python $SCRIPTPATH $dataURL $loginName $loginPass $MOUNTTO $dataMime $outExt
